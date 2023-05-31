d465dbcf59fbf6be6e90074a56eaa8aad72f3285
Fares-Abdelkader <124618781+Fares-Abdelkader@users.noreply.github.com>
2023-05-31 01:27:37 +0000

    js

--- js/script.js
+++ js/script.js
@@ -10,8 +10,9 @@
  * borrowed from Cristiano's ISC2O-unit3-04
  */
 function calculate(input) {
-  if (input == 'fahrenheit') { // If the input is fahrenheit, do fahrenheit to celsius.
-    const fahrenheit = parseFloat(document.getElementById("fahrenheit").value)
-    const answer = ((fahrenheit - 32) * (5 / 9)).toFixed(2)
-    document.getElementById("output-celsius").innerHTML = "<p>This temperature is " + answer + "°C + in fahrenheit</p>"
+  if (input == 'fahrenheit') {
+    const fahrenheit = parseFloat(document.getElementById("fahrenheit").value);
+    const answer = ((fahrenheit - 32) * (5 / 9)).toFixed(2);
+    document.getElementById("output-celsius").innerHTML = "<p>This temperature is " + answer + "°C + in fahrenheit</p>";
   }
+}
